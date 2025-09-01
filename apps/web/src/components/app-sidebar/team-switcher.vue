37a62135017317cd0aff8c83cd6e30f0c6706474
foliageSea <1742262457@qq.com>
2025-09-01 13:50:31 +0800

    feat(web): 侧边栏菜单调整

--- apps/web/src/components/app-sidebar/team-switcher.vue
+++ apps/web/src/components/app-sidebar/team-switcher.vue
@@ -4,17 +4,18 @@ import {
   Plus,
 } from 'lucide-vue-next'
 
-import { useSidebar } from '@/components/ui/sidebar'
+import {useSidebar} from '@/components/ui/sidebar'
 
-import type { Team } from './types'
+import type {Team} from './types'
 
-const { teams } = defineProps<{
+const {teams} = defineProps<{
   teams: Team[]
 }>()
 
-const { isMobile, open } = useSidebar()
+const {isMobile, open} = useSidebar()
 
 const activeTeam = ref<Team>(teams[0])
+
 function setActiveTeam(team: Team) {
   activeTeam.value = team
 }
@@ -40,59 +41,59 @@ function handleSelect(command: TComponent) {
         <UiDropdownMenu>
           <UiDropdownMenuTrigger as-child>
             <UiSidebarMenuButton
-              size="lg"
-              class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                size="lg"
+                class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <div
-                class="flex items-center justify-center rounded-lg aspect-square size-8 bg-sidebar-primary text-sidebar-primary-foreground"
+                  class="flex items-center justify-center rounded-lg aspect-square size-8 bg-sidebar-primary text-sidebar-primary-foreground"
               >
-                <component :is="activeTeam.logo" class="size-4" />
+                <component :is="activeTeam.logo" class="size-4"/>
               </div>
               <div class="grid flex-1 text-sm leading-tight text-left">
                 <span class="font-semibold truncate">{{ activeTeam.name }}</span>
                 <span class="text-xs truncate">{{ activeTeam.plan }}</span>
               </div>
-              <ChevronsUpDown class="ml-auto" />
+              <ChevronsUpDown class="ml-auto"/>
             </UiSidebarMenuButton>
           </UiDropdownMenuTrigger>
           <UiDropdownMenuContent
-            class="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-            align="start"
-            :side="(isMobile || open) ? 'bottom' : 'right'"
-            :side-offset="4"
+              class="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
+              align="start"
+              :side="(isMobile || open) ? 'bottom' : 'right'"
+              :side-offset="4"
           >
             <UiDropdownMenuLabel class="text-xs text-muted-foreground">
               Teams
             </UiDropdownMenuLabel>
             <UiDropdownMenuItem
-              v-for="(team, index) in teams"
-              :key="team.name"
-              class="gap-2 p-2"
-              @click="setActiveTeam(team)"
+                v-for="(team, index) in teams"
+                :key="team.name"
+                class="gap-2 p-2"
+                @click="setActiveTeam(team)"
             >
               <div class="flex items-center justify-center border rounded-sm size-6">
-                <component :is="team.logo" class="size-4 shrink-0" />
+                <component :is="team.logo" class="size-4 shrink-0"/>
               </div>
               {{ team.name }}
               <UiDropdownMenuShortcut>⌘{{ index + 1 }}</UiDropdownMenuShortcut>
             </UiDropdownMenuItem>
-            <UiDropdownMenuSeparator />
+            <UiDropdownMenuSeparator/>
 
-            <UiDialogTrigger as-child>
-              <UiDropdownMenuItem class="gap-2 p-2" @click.stop="handleSelect('team-add')">
-                <div class="flex items-center justify-center border rounded-md size-6 bg-background">
-                  <Plus class="size-4" />
-                </div>
-                <div class="font-medium text-muted-foreground">
-                  Add team
-                </div>
-              </UiDropdownMenuItem>
-            </UiDialogTrigger>
+            <!--            <UiDialogTrigger as-child>-->
+            <!--              <UiDropdownMenuItem class="gap-2 p-2" @click.stop="handleSelect('team-add')">-->
+            <!--                <div class="flex items-center justify-center border rounded-md size-6 bg-background">-->
+            <!--                  <Plus class="size-4" />-->
+            <!--                </div>-->
+            <!--                <div class="font-medium text-muted-foreground">-->
+            <!--                  Add team-->
+            <!--                </div>-->
+            <!--              </UiDropdownMenuItem>-->
+            <!--            </UiDialogTrigger>-->
           </UiDropdownMenuContent>
         </UiDropdownMenu>
 
         <UiDialogContent>
-          <component :is="showComponent" @close="isOpen = false" />
+          <component :is="showComponent" @close="isOpen = false"/>
         </UiDialogContent>
       </UiDialog>
     </UiSidebarMenuItem>
--- apps/web/src/pages/auth/components/auth-title.vue
+++ apps/web/src/pages/auth/components/auth-title.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { useColorMode } from '@vueuse/core'
+import {useColorMode} from '@vueuse/core'
 
 const mode = useColorMode()
 const logo = computed(() => {
@@ -10,7 +10,7 @@ const logo = computed(() => {
 <template>
   <h1 class="flex items-center gap-2 text-xl font-bold">
     <img :src="logo">
-    <span>Shadcn Vue Admin</span>
+    <span>Flutter Template Admin</span>
   </h1>
 </template>
 
